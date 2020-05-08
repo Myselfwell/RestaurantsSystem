2430975d4add6df9d21662624d56318d606be0e3
CristinaZN <710859414@qq.com>
2020-05-08 19:17:40 +0800

    remove unused lib

--- src/Boundary/StaffOption.java
+++ src/Boundary/StaffOption.java
@@ -1,14 +1,8 @@
 package Boundary;
 
-import jdk.jfr.Event;
-
-import java.awt.EventQueue;
-
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
