814a24715a83c7b800b0e9833524a9172e195e50
CristinaZN <710859414@qq.com>
2020-05-28 12:10:29 +0800

    delete [btn change name], add 2 textfield

--- src/Boundary/StaffModifyMenu.java
+++ src/Boundary/StaffModifyMenu.java
@@ -11,10 +11,16 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import javax.swing.JTextField;
+import javax.swing.JTextPane;
+import javax.swing.JToolBar;
+import javax.swing.SwingConstants;
 
 public class StaffModifyMenu extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -51,23 +57,49 @@ public class StaffModifyMenu extends JFrame {
 		JLabel lblNewLabel = new JLabel("Menu");
 		panel.add(lblNewLabel);
 		
-		JButton btnChangeName = new JButton("Change Name");
-		btnChangeName.setBounds(415, 35, 158, 42);
-		contentPane.add(btnChangeName);
-		
 		JButton btnChangePrice = new JButton("Change Price");
-		btnChangePrice.setBounds(415, 110, 158, 42);
+		btnChangePrice.setBounds(415, 193, 158, 42);
 		contentPane.add(btnChangePrice);
-		
+		btnChangePrice.addActionListener(this::btnChangePricePerformed);
+
 		JButton btnBack = new JButton("Back");
 		btnBack.addActionListener(this::btnBackActionPerformed);
-		btnBack.setBounds(415, 185, 158, 42);
+		btnBack.setBounds(415, 245, 158, 42);
 		contentPane.add(btnBack);
 		
 		JButton btnExit = new JButton("Exit");
 		btnExit.addActionListener(this::btnExitActionPerformed);
-		btnExit.setBounds(415, 260, 158, 42);
+		btnExit.setBounds(415, 297, 158, 42);
 		contentPane.add(btnExit);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setToolTipText("");
+		panel_1.setBounds(415, 21, 158, 58);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("name");
+		lblNewLabel_1.setBounds(67, 0, 26, 29);
+		panel_1.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(10, 27, 138, 21);
+		panel_1.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_1_1 = new JPanel();
+		panel_1_1.setBounds(415, 89, 158, 58);
+		contentPane.add(panel_1_1);
+		panel_1_1.setLayout(null);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("New Price");
+		lblNewLabel_1_1.setBounds(57, 10, 58, 15);
+		panel_1_1.add(lblNewLabel_1_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(14, 27, 134, 21);
+		textField_1.setColumns(10);
+		panel_1_1.add(textField_1);
 	}
 
 	private void btnBackActionPerformed(ActionEvent actionEvent) {
@@ -88,5 +120,7 @@ public class StaffModifyMenu extends JFrame {
 		System.exit(0);
 	}
 
+	private void btnChangePricePerformed(ActionEvent actionEvent){
 
+	}
 }
